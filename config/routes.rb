21372d944726075c1aa71bfa66c3e6cf1ebc7c3c
sameehkhan <sameehkhan1@gmail.com>
2018-12-07 14:54:52 -0800

    Generate users and session model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :api, defaults: {format: :json} do
+    resource :session, only: [:create, :destroy]
+    resources :users, only: [:create]
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'static_pages#root'
