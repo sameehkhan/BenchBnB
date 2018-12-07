9b6499f11785da6dfef0ce148df7e7cb3cf225d2
sameehkhan <sameehkhan1@gmail.com>
2018-12-07 15:31:39 -0800

    Refactor

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,8 +1,21 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      redirect_to root_url
+    else
+      render json: ["Invalid credentials"], status: 401
+      redirect_to new_session_url
+    end
   end
 
   def destroy
-    render json: {}
+    if current_user
+      logout
+      render json: {}
+    else
+      render json: ["Not logged in"], status: 404
+    end
   end
 end
