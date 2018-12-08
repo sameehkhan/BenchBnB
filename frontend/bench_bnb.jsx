76fdaa1bbdcdd9c87852146124e7ac513eb8cc09
sameehkhan <sameehkhan1@gmail.com>
2018-12-07 16:12:46 -0800

    Finish auth

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as AU from './util/session_api_util';
+
+window.postUser = AU.postUser;
+window.postSession= AU.postSession;
+window.deleteSession= AU.deleteSession;
 
 document.addEventListener("DOMContentLoaded" , () => {
   const root = document.getElementById("root");
