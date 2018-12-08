c606d5d48485ea837a76b800688f43ebe164f71b
sameehkhan <sameehkhan1@gmail.com>
2018-12-07 17:27:24 -0800

    Finish day 1

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as AU from './util/session_api_util';
+import configureStore from './store/store';
 
 window.postUser = AU.postUser;
 window.postSession= AU.postSession;
 window.deleteSession= AU.deleteSession;
 
 document.addEventListener("DOMContentLoaded" , () => {
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch; // just for testing!
   const root = document.getElementById("root");
   ReactDOM.render(<h1>React</h1>, root);
 });
